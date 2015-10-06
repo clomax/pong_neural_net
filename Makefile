b3cdac8c64c63bde4c2014a088dbea4efd21c8c3
Craig Lomax <craig@clomax.me.uk>
2015-10-06 20:12:00 +0100

    Finally figured out how scaling works.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ OBJS = src/pung.cpp
 OBJ_NAME = build/pung
 
 CC = clang++
-C_FLAGS = -Wall -std=c++11
+C_FLAGS = -Wall -std=c++14
 L_FLAGS = -lsfml-graphics -lsfml-window -lsfml-system -lBox2D
 INCLUDE = -I/usr/include/SFML -I/usr/local/include/Box2D
 
--- src/pung.cpp
+++ src/pung.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 550
-#define METERS_PER_PIXEL 15
-#define PIXELS_PER_METER 1 / METERS_PER_PIXEL
+#define SCALE 30.f
 
 struct paddle
 {
+  std::string name;
   sf::RectangleShape *shape;
   sf::Vector2f pos;
   sf::Vector2f dim;
   sf::Color colour;
+  b2Body *body;
 };
 
 struct ball
 {
-  sf::CircleShape *shape;
+  std::string name;
   b2Body *body;
+  sf::CircleShape *shape;
   sf::Color colour;
   float radius;
 };
@@ -33,68 +36,106 @@ main (int argc, char ** argv)
   b2World World(Gravity);
 
   // Create player paddle
-  paddle *p1 = (paddle *) malloc(sizeof(paddle));
-  p1->dim = sf::Vector2f(20,100);
-  p1->pos = sf::Vector2f(50, (SCREEN_HEIGHT / 2) - (p1->dim.y / 2));
-  p1->colour = sf::Color::Yellow;
+  paddle p1;
+  p1.name = std::string("Paddle 1");
+  p1.dim = sf::Vector2f(20,100);
+  p1.pos = sf::Vector2f(
+    50,
+    (SCREEN_HEIGHT / 2) - (p1.pos.y / 2));
+  p1.colour = sf::Color::Yellow;
+
+  b2BodyDef p1_p1_paddle_body_def;
+  p1_p1_paddle_body_def.position = b2Vec2(p1.pos.x/SCALE, p1.pos.y/SCALE);
+  p1_p1_paddle_body_def.type = b2_staticBody;
+  b2Body* p1_paddle_body = World.CreateBody(&p1_p1_paddle_body_def);
+  p1.body = p1_paddle_body;
+
+  b2PolygonShape p1_b2_paddle_shape;
+  p1_b2_paddle_shape.SetAsBox((p1.dim.x/2)/SCALE, (p1.dim.y/2)/SCALE);
+
+  b2FixtureDef p1_b2_paddle_fixture_def;
+  p1_b2_paddle_fixture_def.shape = &p1_b2_paddle_shape;
+  p1_b2_paddle_fixture_def.density = 1.f;
+  p1.body->CreateFixture(&p1_b2_paddle_fixture_def);
 
   sf::RectangleShape p1_shape;
-  p1->shape = &p1_shape;
-  p1->shape->setSize(sf::Vector2f(p1->dim.x, p1->dim.y));
-  p1->shape->setFillColor(p1->colour);
+  p1.shape = &p1_shape;
+  p1.shape->setOrigin(sf::Vector2f(p1.dim.x/2, p1.dim.y/2));
+  p1.shape->setPosition(sf::Vector2f(p1.pos.x, p1.pos.y));
+  p1.shape->setSize(sf::Vector2f(p1.dim.x, p1.dim.y));
+  p1.shape->setFillColor(p1.colour);
 
 
   // Create AI paddle
-  paddle *p2 = (paddle *) malloc(sizeof(paddle));
-  p2->dim = sf::Vector2f(20, 100);
-  p2->pos = sf::Vector2f((SCREEN_WIDTH - 50), (SCREEN_HEIGHT / 2) - (p2->dim.y / 2));
-  p2->colour = sf::Color::Yellow;
+  paddle p2;
+  p2.name = std::string("Paddle 2");
+  p2.dim = sf::Vector2f(20,100);
+  p2.pos = sf::Vector2f(
+    (SCREEN_WIDTH - 50),
+    (SCREEN_HEIGHT / 2) - (p2.dim.y / 2)
+  );
+  p2.colour = sf::Color::Yellow;
+
+  b2BodyDef paddle_body_def;
+  paddle_body_def.position = b2Vec2(p2.pos.x/SCALE, p2.pos.y/SCALE);
+  paddle_body_def.type = b2_staticBody;
+  b2Body* paddle_body = World.CreateBody(&paddle_body_def);
+  p2.body = paddle_body;
+
+  b2PolygonShape b2_paddle_shape;
+  b2_paddle_shape.SetAsBox((p2.dim.x/2)/SCALE, (p2.dim.y/2)/SCALE);
+
+  b2FixtureDef b2_paddle_fixture_def;
+  b2_paddle_fixture_def.shape = &b2_paddle_shape;
+  b2_paddle_fixture_def.density = 1.f;
+  p2.body->CreateFixture(&b2_paddle_fixture_def);
 
   sf::RectangleShape p2_shape;
-  p2->shape = &p2_shape;
-  p2->shape->setSize(sf::Vector2f(p2->dim.x, p2->dim.y));
-  p2->shape->setFillColor(p1->colour);
+  p2.shape = &p2_shape;
+  p2.shape->setOrigin(sf::Vector2f(p2.dim.x/2, p2.dim.y/2));
+  p2.shape->setPosition(sf::Vector2f(p2.pos.x, p2.pos.y));
+  p2.shape->setSize(sf::Vector2f(p2.dim.x, p2.dim.y));
+  p2.shape->setFillColor(p2.colour);
 
 
   // Create divider
   sf::RectangleShape divider;
   divider.setSize(sf::Vector2f(10, SCREEN_HEIGHT));
   divider.setPosition(sf::Vector2f((SCREEN_WIDTH / 2) + 5, 0));
-
+  divider.setFillColor(sf::Color(200,200,200));
 
   // Create ball 0
-  ball *ball0 = (ball *) malloc(sizeof(ball));
-  ball0->radius = 10.0f;
-  ball0->colour = sf::Color::Red;
+  ball ball0;
+  ball0.name = "Ball";
+  ball0.radius = 5.0f;
+  ball0.colour = sf::Color::Red;
+
+  sf::CircleShape ball_shape;
+  ball0.shape = &ball_shape;
+  ball0.shape->setRadius(ball0.radius);
+  ball0.shape->setOrigin(sf::Vector2f(ball0.radius/2, ball0.radius/2));
+  ball0.shape->setFillColor(ball0.colour);
 
   b2BodyDef ball_body_def;
-  ball_body_def.position = b2Vec2(
-    (SCREEN_WIDTH / 2) * PIXELS_PER_METER,
-    (SCREEN_HEIGHT / 2)
-  );
+  ball_body_def.position = b2Vec2(10/SCALE,10/SCALE);
   ball_body_def.type = b2_dynamicBody;
 
   b2Body* ball_body = World.CreateBody(&ball_body_def);
-  ball0->body = ball_body;
+  ball0.body = ball_body;
+
+  ball_body->SetUserData(&ball0);
 
   b2CircleShape b2_ball_shape;
-  b2_ball_shape.m_p.Set(0,0);
-  b2_ball_shape.m_radius = ball0->radius;
+  b2_ball_shape.m_radius = ball0.radius/SCALE;
 
   b2FixtureDef b2_ball_fixture_def;
   b2_ball_fixture_def.shape = &b2_ball_shape;
-  b2_ball_fixture_def.density = 100.f;
-  ball0->body->CreateFixture(&b2_ball_fixture_def);
+  b2_ball_fixture_def.density = 1.f;
+  b2_ball_fixture_def.restitution = 1.f;
+  ball0.body->CreateFixture(&b2_ball_fixture_def);
+  ball0.body->SetTransform(b2Vec2((SCREEN_WIDTH/2)/SCALE, (SCREEN_HEIGHT/2)/SCALE), 0);
 
-  sf::CircleShape ball_shape;
-  ball0->shape = &ball_shape;
-  ball0->shape->setRadius(ball0->radius);
-  ball0->shape->setOrigin(sf::Vector2f(ball0->radius/2, ball0->radius/2));
-  ball0->shape->setFillColor(ball0->colour);
-
-  b2Vec2 vel = ball0->body->GetLinearVelocity();
-  vel.x = -3 * METERS_PER_PIXEL;
-  ball0->body->SetLinearVelocity(vel);
+  ball0.body->SetLinearVelocity(b2Vec2(-10.f,0.f));
 
   sf::Clock clk;
   sf::Time dt;
@@ -102,8 +143,6 @@ main (int argc, char ** argv)
   while (window.isOpen())
   {
     dt = clk.restart();
-
-    window.clear(sf::Color(128,128,128));
     World.Step(1/60.f, 8, 3);
 
     sf::Event event;
@@ -113,54 +152,47 @@ main (int argc, char ** argv)
         window.close();
     }
 
+    b2Vec2 p1_position = p1.body->GetPosition();
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
-      if (p1->pos.y >= 0)
-        p1->pos.y -= 350.0f * dt.asSeconds();
+      if (p1_position.y >= (p1.dim.y/2) / SCALE)
+      {
+        p1_position.y -= 5.f * dt.asSeconds();
+        p1.body->SetTransform(p1_position, 0);
+      }
     }
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
     {
-      if (p1->pos.y <= (SCREEN_HEIGHT - p1->dim.y))
-        p1->pos.y += 350.0f * dt.asSeconds();
+      if (p1_position.y <= (SCREEN_HEIGHT / SCALE) - ((p1.dim.y/2) / SCALE))
+      {
+        p1_position.y += 5.f * dt.asSeconds();
+        p1.body->SetTransform(p1_position, 0);
+
+      }
     }
 
-    p1->shape->setPosition(p1->pos.x, p1->pos.y);
-    p2->shape->setPosition(p2->pos.x, p2->pos.y);
+    window.clear(sf::Color(128,128,128));
 
-    ball0->shape->setPosition(
-      ball0->body->GetPosition().x * METERS_PER_PIXEL,
-      ball0->body->GetPosition().y
-    );
+    p1.shape->setPosition(
+      SCALE * p1.body->GetPosition().x,
+      SCALE * p1.body->GetPosition().y);
 
-    if(ball0->body->GetPosition().x < 0)
+    ball0.shape->setPosition(
+      SCALE * ball0.body->GetPosition().x,
+      SCALE * ball0.body->GetPosition().y);
+
+    for (b2Body* BodyIterator = World.GetBodyList();
+         BodyIterator !=0;
+         BodyIterator = BodyIterator->GetNext())
     {
-      ball0->body->SetTransform(b2Vec2(
-        SCREEN_WIDTH/2 * PIXELS_PER_METER,
-        SCREEN_HEIGHT/2),
-        0
-      );
-      b2Vec2 vel = b2Vec2(3*METERS_PER_PIXEL,0);
-      ball0->body->SetLinearVelocity(vel);
+
     }
 
-    if(ball0->body->GetPosition().x > SCREEN_WIDTH * PIXELS_PER_METER)
-    {
-      ball0->body->SetTransform(b2Vec2(
-        SCREEN_WIDTH/2 * PIXELS_PER_METER,
-        SCREEN_HEIGHT/2),
-        0
-      );
-      b2Vec2 vel = b2Vec2(-3*METERS_PER_PIXEL,0);
-      ball0->body->SetLinearVelocity(vel);
-    }
-
-    //ball0->body->ApplyForceToCenter( b2Vec2(-500,0), true );
-
-    window.draw(*p1->shape);
-    window.draw(*p2->shape);
     window.draw(divider);
-    window.draw(*ball0->shape);
+    window.draw(*p1.shape);
+    window.draw(*p2.shape);
+    window.draw(*ball0.shape);
     window.display();
   }
 
