e793cba7814d3ad5e84d14eb8116d77d8c743272
Craig Lomax <craig@clomax.me.uk>
2015-10-20 11:55:05 +0100

    Put paddles and ball on the heap instead.

--- src/pung.cpp
+++ src/pung.cpp
@@ -39,20 +39,17 @@ typedef ublas::vector<float> vector;
 
 struct paddle
 {
-  std::string name;
   sf::RectangleShape *shape;
   sf::Vector2f pos;
   sf::Vector2f dim;
   sf::Color colour;
   b2Body *body;
-  float speed;
   float target_y;
   int score = 0;
 };
 
 struct ball
 {
-  std::string name;
   b2Body *body;
   sf::CircleShape *shape;
   sf::Color colour;
@@ -76,15 +73,6 @@ random_float (float min, float max)
   float random = ((float) rand()) / (float) RAND_MAX;
   float range = max - min;
   return (random*range) + min;
-}
-
-inline float
-ball_direction_relative_to_paddle(b2Vec2 last_dist, paddle p, ball b)
-{
-  float p_x = p.body->GetPosition().x;
-  float b_x = b.body->GetPosition().x;
-  float val = (std::abs(p_x - b_x) > std::abs(p_x - last_dist.x)) ? -1.f : 1.f;
-  return val;
 }
 
 int
@@ -216,67 +204,66 @@ main (int argc, char ** argv)
   b2World World(Gravity);
 
   // Create player paddle
-  paddle p1;
-  p1.name = std::string("Paddle 1");
-  p1.dim = sf::Vector2f(20,100);
-  p1.pos = sf::Vector2f(
+  paddle* p1 = (paddle*)malloc(sizeof(paddle));
+  p1->dim = sf::Vector2f(20,100);
+  p1->pos = sf::Vector2f(
     50,
-    (SCREEN_HEIGHT / 2) - (p1.pos.y / 2));
-  p1.colour = sf::Color::Yellow;
+    (SCREEN_HEIGHT / 2) - (p1->pos.y / 2));
+  p1->colour = sf::Color::Yellow;
+  p1->score = 0;
 
   b2BodyDef p1_p1_paddle_body_def;
-  p1_p1_paddle_body_def.position = b2Vec2(p1.pos.x/SCALE, p1.pos.y/SCALE);
+  p1_p1_paddle_body_def.position = b2Vec2(p1->pos.x/SCALE, p1->pos.y/SCALE);
   p1_p1_paddle_body_def.type = b2_staticBody;
   b2Body* p1_paddle_body = World.CreateBody(&p1_p1_paddle_body_def);
-  p1.body = p1_paddle_body;
+  p1->body = p1_paddle_body;
 
   b2PolygonShape p1_b2_paddle_shape;
-  p1_b2_paddle_shape.SetAsBox((p1.dim.x/2)/SCALE, (p1.dim.y/2)/SCALE);
+  p1_b2_paddle_shape.SetAsBox((p1->dim.x/2)/SCALE, (p1->dim.y/2)/SCALE);
 
   b2FixtureDef p1_b2_paddle_fixture_def;
   p1_b2_paddle_fixture_def.shape = &p1_b2_paddle_shape;
   p1_b2_paddle_fixture_def.density = 1.f;
-  p1.body->CreateFixture(&p1_b2_paddle_fixture_def);
+  p1->body->CreateFixture(&p1_b2_paddle_fixture_def);
 
   sf::RectangleShape p1_shape;
-  p1.shape = &p1_shape;
-  p1.shape->setOrigin(sf::Vector2f(p1.dim.x/2, p1.dim.y/2));
-  p1.shape->setPosition(sf::Vector2f(p1.pos.x, p1.pos.y));
-  p1.shape->setSize(sf::Vector2f(p1.dim.x, p1.dim.y));
-  p1.shape->setFillColor(p1.colour);
+  p1->shape = &p1_shape;
+  p1->shape->setOrigin(sf::Vector2f(p1->dim.x/2, p1->dim.y/2));
+  p1->shape->setPosition(sf::Vector2f(p1->pos.x, p1->pos.y));
+  p1->shape->setSize(sf::Vector2f(p1->dim.x, p1->dim.y));
+  p1->shape->setFillColor(p1->colour);
 
 
   // Create AI paddle
-  paddle p2;
-  p2.name = std::string("Paddle 2");
-  p2.dim = sf::Vector2f(20,100);
-  p2.pos = sf::Vector2f(
+  paddle* p2 = (paddle*)malloc(sizeof(paddle));
+  p2->dim = sf::Vector2f(20,100);
+  p2->pos = sf::Vector2f(
     (SCREEN_WIDTH - 50),
     (SCREEN_HEIGHT / 2)
   );
-  p2.colour = opponent_colour;
-  p2.speed = 1.f;
+  p2->colour = opponent_colour;
+  p2->score = 0;
 
   b2BodyDef paddle_body_def;
-  paddle_body_def.position = b2Vec2(p2.pos.x/SCALE, p2.pos.y/SCALE);
+  paddle_body_def.position = b2Vec2(p2->pos.x/SCALE, p2->pos.y/SCALE);
   paddle_body_def.type = b2_staticBody;
   b2Body* paddle_body = World.CreateBody(&paddle_body_def);
-  p2.body = paddle_body;
+  p2->body = paddle_body;
 
   b2PolygonShape b2_paddle_shape;
-  b2_paddle_shape.SetAsBox((p2.dim.x/2)/SCALE, (p2.dim.y/2)/SCALE);
+  b2_paddle_shape.SetAsBox((p2->dim.x/2)/SCALE, (p2->dim.y/2)/SCALE);
 
   b2FixtureDef b2_paddle_fixture_def;
   b2_paddle_fixture_def.shape = &b2_paddle_shape;
   b2_paddle_fixture_def.density = 1.f;
-  p2.body->CreateFixture(&b2_paddle_fixture_def);
+  p2->body->CreateFixture(&b2_paddle_fixture_def);
 
   sf::RectangleShape p2_shape;
-  p2.shape = &p2_shape;
-  p2.shape->setOrigin(sf::Vector2f(p2.dim.x/2, p2.dim.y/2));
-  p2.shape->setPosition(sf::Vector2f(p2.pos.x, p2.pos.y));
-  p2.shape->setSize(sf::Vector2f(p2.dim.x, p2.dim.y));
-  p2.shape->setFillColor(p2.colour);
+  p2->shape = &p2_shape;
+  p2->shape->setOrigin(sf::Vector2f(p2->dim.x/2, p2->dim.y/2));
+  p2->shape->setPosition(sf::Vector2f(p2->pos.x, p2->pos.y));
+  p2->shape->setSize(sf::Vector2f(p2->dim.x, p2->dim.y));
+  p2->shape->setFillColor(p2->colour);
 
 
   // Create divider
@@ -285,41 +272,40 @@ main (int argc, char ** argv)
   divider.setPosition(sf::Vector2f((SCREEN_WIDTH / 2) + 5, 0));
   divider.setFillColor(sf::Color(200,200,200));
 
-  // Create ball 0
-  ball ball0;
-  ball0.name = "Ball";
-  ball0.radius = 20.0f;
-  ball0.speed = 25.f;
-  ball0.colour = sf::Color::Red;
+  // Create ball
+  ball* b = (ball*)malloc(sizeof(ball));
+  b->radius = 20.0f;
+  b->speed = 25.f;
+  b->colour = sf::Color::Red;
 
   sf::Texture ball_texture;
   ball_texture.loadFromFile("assets/images/ball.png");
 
   sf::CircleShape ball_shape;
-  ball0.shape = &ball_shape;
-  ball0.shape->setRadius(ball0.radius);
-  ball0.shape->setOrigin(sf::Vector2f((ball0.radius),(ball0.radius)));
-  ball0.shape->setTexture(&ball_texture);
+  b->shape = &ball_shape;
+  b->shape->setRadius(b->radius);
+  b->shape->setOrigin(sf::Vector2f((b->radius),(b->radius)));
+  b->shape->setTexture(&ball_texture);
 
   b2BodyDef ball_body_def;
   ball_body_def.position = b2Vec2(10/SCALE,10/SCALE);
   ball_body_def.type = b2_dynamicBody;
 
   b2Body* ball_body = World.CreateBody(&ball_body_def);
-  ball0.body = ball_body;
+  b->body = ball_body;
 
-  ball_body->SetUserData(&ball0);
+  ball_body->SetUserData(&b);
 
   b2CircleShape b2_ball_shape;
-  b2_ball_shape.m_radius = ball0.radius/SCALE;
+  b2_ball_shape.m_radius = b->radius/SCALE;
 
   b2FixtureDef b2_ball_fixture_def;
   b2_ball_fixture_def.shape = &b2_ball_shape;
   b2_ball_fixture_def.density = 1.f;
   b2_ball_fixture_def.restitution = 1.f;
-  ball0.body->CreateFixture(&b2_ball_fixture_def);
-  ball0.body->SetTransform(b2Vec2((SCREEN_WIDTH/2)/SCALE, (SCREEN_HEIGHT/2)/SCALE), 0);
-  ball0.body->SetLinearVelocity(b2Vec2(-ball0.speed, random_float(-3.f, 3.f)));
+  b->body->CreateFixture(&b2_ball_fixture_def);
+  b->body->SetTransform(b2Vec2((SCREEN_WIDTH/2)/SCALE, (SCREEN_HEIGHT/2)/SCALE), 0);
+  b->body->SetLinearVelocity(b2Vec2(-b->speed, random_float(-3.f, 3.f)));
 
   // create paddle score text things
   sf::Font score_font;
@@ -331,14 +317,14 @@ main (int argc, char ** argv)
 
   sf::Text p1_score;
   p1_score.setFont(score_font);
-  p1_score.setString(std::to_string(p1.score));
+  p1_score.setString(std::to_string(p1->score));
   p1_score.setCharacterSize(32);
   p1_score.setPosition((SCREEN_WIDTH/2) - 50,10);
   p1_score.setOrigin(p1_score.getScale()/2.f);
 
   sf::Text p2_score;
   p2_score.setFont(score_font);
-  p2_score.setString(std::to_string(p2.score));
+  p2_score.setString(std::to_string(p2->score));
   p2_score.setCharacterSize(32);
   p2_score.setPosition((SCREEN_WIDTH/2) + 50,10);
   p2_score.setOrigin(p2_score.getScale()/2.f);
@@ -393,33 +379,31 @@ main (int argc, char ** argv)
       window.close();
     }
 
-    ball0.shape->setPosition(
-      SCALE * ball0.body->GetPosition().x,
-      SCALE * ball0.body->GetPosition().y);
+    b->shape->setPosition(
+      SCALE * b->body->GetPosition().x,
+      SCALE * b->body->GetPosition().y);
 
     float dist;
-    b2Vec2 ball_v = ball0.body->GetLinearVelocity();
-    float ball_y = ((ball0.body->GetPosition().y) / (SCREEN_HEIGHT/SCALE)) * 100;
+    b2Vec2 ball_v = b->body->GetLinearVelocity();
+    float ball_y = ((b->body->GetPosition().y) / (SCREEN_HEIGHT/SCALE)) * 100;
     float mouse_y = sf::Mouse::getPosition(window).y;
 
-    if (mouse_y >= p1.dim.y / 2 && mouse_y <= SCREEN_HEIGHT - (p1.dim.y / 2))
-      p1.target_y = sf::Mouse::getPosition(window).y / SCALE;
-      //p1.target_y = (SCREEN_HEIGHT/2) / SCALE;
-      //p1.target_y = ball0.body->GetPosition().y;
+    if (mouse_y >= p1->dim.y / 2 && mouse_y <= SCREEN_HEIGHT - (p1->dim.y / 2))
+      p1->target_y = sf::Mouse::getPosition(window).y / SCALE;
 
-    b2Vec2 p1_position = p1.body->GetPosition();
-    p1_position.y = p1.target_y - (p1.target_y - p1_position.y) * damping;
-    p1.body->SetTransform(p1_position, 0);
+    b2Vec2 p1_position = p1->body->GetPosition();
+    p1_position.y = p1->target_y - (p1->target_y - p1_position.y) * damping;
+    p1->body->SetTransform(p1_position, 0);
 
-    p1.shape->setPosition(
-      SCALE * p1.body->GetPosition().x,
-      SCALE * p1.body->GetPosition().y);
+    p1->shape->setPosition(
+      SCALE * p1->body->GetPosition().x,
+      SCALE * p1->body->GetPosition().y);
 
-    b2Vec2 p2_position = p2.body->GetPosition();
+    b2Vec2 p2_position = p2->body->GetPosition();
 
     if (playmode == 1)
     {
-      dist = std::abs(ball0.body->GetPosition().x - p2.body->GetPosition().x);
+      dist = std::abs(b->body->GetPosition().x - p2->body->GetPosition().x);
       vector a1(inputs);
       a1(0) = 1;
       a1(1) = dist;
@@ -440,7 +424,7 @@ main (int argc, char ** argv)
 
       vector h = sigmoid(prod(a2, Theta2));
 
-      float p2_target = ball0.body->GetPosition().y;
+      float p2_target = b->body->GetPosition().y;
       p2_target = (h(0) * SCREEN_HEIGHT/SCALE);
       p2_position.y = p2_target - (p2_target - p2_position.y) * damping;
 
@@ -448,81 +432,85 @@ main (int argc, char ** argv)
     }
     else
     {
-      dist = std::abs(ball0.body->GetPosition().x - p1.body->GetPosition().x);
+      dist = std::abs(b->body->GetPosition().x - p1->body->GetPosition().x);
 
-      p2_position = p2.body->GetPosition();
-      float p2_target = ball0.body->GetPosition().y;
+      p2_position = p2->body->GetPosition();
+      float p2_target = b->body->GetPosition().y;
       p2_position.y = p2_target - (p2_target - p2_position.y) * damping;
     }
 
-    p2.body->SetTransform(p2_position, 0);
+    p2->body->SetTransform(p2_position, 0);
 
-    p2.shape->setPosition(
-      SCALE * p2.body->GetPosition().x,
-      SCALE * p2.body->GetPosition().y);
+    p2->shape->setPosition(
+      SCALE * p2->body->GetPosition().x,
+      SCALE * p2->body->GetPosition().y);
 
-    float vert_velocity = ball0.body->GetLinearVelocity().y;
+    float vert_velocity = b->body->GetLinearVelocity().y;
     if (std::abs(vert_velocity) < 3.f)
     {
       float new_vert_velocity;
       new_vert_velocity = (vert_velocity < 0) ? -2.f : 2.f;
       b2Vec2 ball_vertical_velocity = b2Vec2(0.f, new_vert_velocity);
-      ball0.body->ApplyForceToCenter(ball_vertical_velocity, true);
+      b->body->ApplyForceToCenter(ball_vertical_velocity, true);
     }
 
-    float horiz_velocity = ball0.body->GetLinearVelocity().x;
-    if (std::abs(horiz_velocity) < ball0.speed)
+    float horiz_velocity = b->body->GetLinearVelocity().x;
+    if (std::abs(horiz_velocity) < b->speed)
     {
       float new_horiz_velocity;
       new_horiz_velocity = (horiz_velocity < 0) ? -5.f : 5.f;
       b2Vec2 ball_horizical_velocity = b2Vec2(new_horiz_velocity, 0.f);
-      ball0.body->ApplyForceToCenter(ball_horizical_velocity, true);
+      b->body->ApplyForceToCenter(ball_horizical_velocity, true);
     }
 
-    if (ball0.body->GetPosition().x < 0 || ball0.body->GetPosition().x > (SCREEN_WIDTH / SCALE))
+    if (b->body->GetPosition().x < 0 || b->body->GetPosition().x > (SCREEN_WIDTH / SCALE))
     {
-      if (ball0.body->GetPosition().x < 0)
-        p2.score += 1;
+      if (b->body->GetPosition().x < 0)
+        p2->score += 1;
       else
-        p1.score += 1;
+        p1->score += 1;
 
       float choices[2] = {-1.f, 1.f};
-      std::srand(time(NULL));
+      std::srand(time(nullptr));
       int r = rand()%2;
       float c = choices[r];
-      ball0.body->SetTransform(b2Vec2((SCREEN_WIDTH/2)/SCALE, (SCREEN_HEIGHT/2)/SCALE), 0);
-      ball0.body->SetLinearVelocity(b2Vec2(
-        ball0.speed * c,
-        random_float(-ball0.speed, ball0.speed)));
+      b->body->SetTransform(b2Vec2((SCREEN_WIDTH/2)/SCALE, (SCREEN_HEIGHT/2)/SCALE), 0);
+      b->body->SetLinearVelocity(b2Vec2(
+        b->speed * c,
+        random_float(-b->speed, b->speed)));
     }
 
-    //ball0.body->SetAngularVelocity(0.f);
-    ball0.shape->setRotation(ball0.body->GetAngle() * (180 / PI));
+    //b->body->SetAngularVelocity(0.f);
+    b->shape->setRotation(b->body->GetAngle() * (180 / PI));
 
     // write human data to file
     if (playmode == 0)
     {
-      float target_y = (p1.target_y / (SCREEN_HEIGHT/SCALE)) * 100.f;
+      float target_y = (p1->target_y / (SCREEN_HEIGHT/SCALE)) * 100.f;
 
       std::ostringstream buff;
       buff << dist << delim << ball_y << delim << ball_v.x << delim << ball_v.y << delim << target_y << "\n";
       human_file << buff.str();
     }
 
-    p1_score.setString(std::to_string(p1.score));
-    p2_score.setString(std::to_string(p2.score));
+    p1_score.setString(std::to_string(p1->score));
+    p2_score.setString(std::to_string(p2->score));
 
     window.clear(sf::Color(110,110,110));
     window.draw(p1_score);
     window.draw(p2_score);
     window.draw(divider);
-    window.draw(*p1.shape);
-    window.draw(*p2.shape);
-    window.draw(*ball0.shape);
+    window.draw(*p1->shape);
+    window.draw(*p2->shape);
+    window.draw(*b->shape);
     window.display();
 
     dt = clk.restart();
   }
+
+  free(p1);
+  free(p2);
+  free(b);
 
   if (human_file.is_open())
     human_file.close();
